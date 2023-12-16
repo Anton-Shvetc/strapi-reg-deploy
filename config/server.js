89c6326cd2b3e18b45c552b437654f8b79702dc3
Anton <antonshvetswork@yandex.ru>
2023-12-16 18:38:30 +0300

    Change cors

--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 443),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 443),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+  url: "https://darkmode-serve.ru/",
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
